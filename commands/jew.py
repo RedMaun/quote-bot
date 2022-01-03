43aa414fa212d303b3990942625e07c04181e212
RedMaun <deb.il.dpkg@gmail.com>
2022-01-03 18:29:04 +0700

    jew

--- commands/jew.py
+++ commands/jew.py
@@ -6,25 +6,37 @@ bp = Blueprint()
 
 class Command(AbstractCommand):
     def __init__(self):
-        super().__init__(handler = ['/еврей <k> <item>', '/ЕВРЕЙ <k> <item>', '/еврей <item>', '/ЕВРЕЙ <item>'], description = 'jewificator v1.756')
+        super().__init__(handler = ['/еврей <k> <item>', '/ЕВРЕЙ <k> <item>', '/еврей <item>', '/ЕВРЕЙ <item>', '/еврей', '/ЕВРЕЙ'], description = 'jewificator v1.756')
 
 Jew = Command()
 
 @bp.on.message(text = Jew.hdl())
 async def jewificator(m: Message, k: Optional[str] = None, item: Optional[int] = None):
     try:
-        if k == None:
-            answer = "("*3 + item + ")"*3
-            await Jew.ans_up(answer, m)
-        elif int(k) < 100 and int(k) > 0:
-            k = int(k)
-            if isinstance(k, int):
-                answer = "("*k + item + ")"*k
+        if m.reply_message:
+            k = item
+            print(k)
+            if k == None:
+                answer = "("*3 + m.reply_message.text + ")"*3
                 await Jew.ans_up(answer, m)
-            else:
-                await Jew.ans_up('errors', m)
+            elif int(k) < 100 and int(k) > 0:
+                k = int(k)
+                answer = "("*k + m.reply_message.text + ")"*k
+                await Jew.ans_up(answer, m)
         else:
-            await Jew.ans_up('must be int in range (0, 100)', m)
+
+            if k == None:
+                answer = "("*3 + item + ")"*3
+                await Jew.ans_up(answer, m)
+            elif int(k) < 100 and int(k) > 0:
+                k = int(k)
+                if isinstance(k, int):
+                    answer = "("*k + item + ")"*k
+                    await Jew.ans_up(answer, m)
+                else:
+                    await Jew.ans_up('errors', m)
+            else:
+                await Jew.ans_up('must be int in range (0, 100)', m)
     except Exception as e:
         await Jew.ans_up(e, m)
     
