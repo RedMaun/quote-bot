a96e5650357a6274e020796fa164412a7166b41e
RedMaun <deb.il.dpkg@gmail.com>
2022-01-20 11:51:18 +0700

    🤠

--- commands/banner.py
+++ commands/banner.py
@@ -12,7 +12,7 @@ bp = Blueprint()
 
 class Command(AbstractCommand):
     def __init__(self):
-        super().__init__(handler=['/баннер', '/БАННЕР', '/реклама', '/РЕКЛАМА'],
+        super().__init__(handler=['/баннер <item>', '/БАННЕР <item>', '/реклама <item>', '/РЕКЛАМА <item>'],
                          description='will add a banner to quote site')
 
 
@@ -21,15 +21,30 @@ Banner = Command()
 
 @bp.on.message(text=Banner.hdl())
 async def list(m: Message, item: Optional[int] = None):
-    for banner in itertools.chain.from_iterable([x['images'] for x in await unpack(m)]):
-        files_list = os.listdir(config_content['banners_dir'])
-        if len(files_list) < 5:
-            file = os.path.join(config_content['banners_dir'], f'{len(files_list) + 1}.webp')
-        else:
-            file = os.path.join(
-                config_content['banners_dir'],
-                min([(os.stat(os.path.join(config_content['banners_dir'],filename))[stat.ST_CTIME], filename)
-                    for filename in files_list], key=lambda x: x[0])[1])
-            os.remove(file)
-        os.replace(os.path.join(config_content['pics_dir'], banner.split('/')[-1]), file)
-    await Banner.ans_up(config_content['default']['ok'], m)
+    if item == None:
+        for banner in itertools.chain.from_iterable([x['images'] for x in await unpack(m)]):
+            files_list = os.listdir(config_content['banners_dir'])
+            if len(files_list) < 4:
+                file = os.path.join(config_content['banners_dir'], f'{len(files_list) + 1}.webp')
+            else:
+                file = os.path.join(
+                    config_content['banners_dir'],
+                    min([(os.stat(os.path.join(config_content['banners_dir'],filename))[stat.ST_CTIME], filename)
+                        for filename in files_list], key=lambda x: x[0])[1])
+                os.remove(file)
+            os.replace(os.path.join(config_content['pics_dir'], banner.split('/')[-1]), file)
+        await Banner.ans_up(config_content['default']['ok'], m)
+    else:
+        try:
+            item = int(item)
+            if item in range(1, 6):
+                for banner in itertools.chain.from_iterable([x['images'] for x in await unpack(m)]):
+                    files_list = os.listdir(config_content['banners_dir'])
+                    file = os.path.join(config_content['banners_dir'], str(item) + '.webp')
+                    if str(item) + '.webp' in files_list:
+                        os.remove(file)
+                    os.replace(os.path.join(config_content['pics_dir'], banner.split('/')[-1]), file)
+                    await Banner.ans_up(config_content['default']['ok'], m)
+                    break
+        except Exception as e:
+            await Banner.ans_up(e, m)
